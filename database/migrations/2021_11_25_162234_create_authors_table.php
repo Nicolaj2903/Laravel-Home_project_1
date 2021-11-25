fa645f11e1e2f00f4c2a3c0b47e077a7e1787a2e
Nicolaj2903 <Iversen2903@hotmail.com>
2021-11-25 17:25:29 +0100

    Added some tables for the database

---
+++ database/migrations/2021_11_25_162234_create_authors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('author_type');  // Crime-fiction author, horror, fiction?
+            $table->integer('age');
+
+
+            $table->foreign('publisher_id')->references('id')->on('publishers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
